a327d64b31502c5c98743f237611e4d735e2c031
Dinesh <dinesh30mane@gmail.com>
2026-09-06 03:50:33 +0530

    handle CORS related errors

--- sentiment-ai-backend/src/main/java/com/dineshmane/controller/FeedbackController.java
+++ sentiment-ai-backend/src/main/java/com/dineshmane/controller/FeedbackController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/feedback")
+@CrossOrigin(origins = "http://localhost:5173/")
 @RequiredArgsConstructor
 public class FeedbackController {
 
