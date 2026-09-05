e96f23324eadc648255fe3345ebd8fd7546edf88
Dinesh <dinesh30mane@gmail.com>
2026-09-06 01:41:49 +0530

    add feedback history in UI

--- sentiment-ai-frontend/src/App.jsx
+++ sentiment-ai-frontend/src/App.jsx
@@ -15,8 +15,30 @@ function App() {
         </h1>
         <form className="mb-4">
           <textarea className="w-full p-2 border rounded mb-2" placeholder='Enter your Feedback...' rows="4"></textarea>
-          <button type='submit' className='px-4 py-2 rounded bg-blue-600 text-white'>Submit Feedback</button>
+          <button
+            type="submit"
+            className="px-4 py-2 rounded bg-blue-600 text-white">
+            Submit Feedback
+          </button>
         </form>
+
+        <h2 className='text-xl font-semibold mb-2'>Feedback History</h2>
+        <table className='table-auto w-full text-sm text-left text-gray-500'>
+          <thead className='text-gray-700 bg-gray-50'>
+            <tr>
+              <th>Feedback</th>
+              <th>Sentiment Score</th>
+              <th>Sentiment</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>feeback will come here.</td>
+              <td>0.5</td>
+              <td>POSITIVE</td>
+            </tr>
+          </tbody>
+        </table>
 
       </div>
     </>
